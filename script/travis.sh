5dd888c46025deaf5317b268733b9c37346be91f
Chris Drake <cjdrake@gmail.com>
2017-08-01 23:28:28 -0700

    Update Travis script to run unit tests

--- script/travis.sh
+++ script/travis.sh
@@ -1,3 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Hello, world!"
+# Copyright 2017 Chris Drake
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Execute test suite on Travis-CI
+
+set -e  # errexit
+set -x  # xtrace
+
+mkdir build
+cd build
+cmake -G "Unix Makefiles" ..
+
+# Build libraries and binaries
+make
+
+# Execute CTest tests
+make test
